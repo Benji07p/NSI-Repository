92aca418c978a9ed83e0b78fad39bc59634ae19d
Benji07p <82090430+Benji07p@users.noreply.github.com>
2025-03-03 14:38:00 +0100

    Ex4 Correction

--- ex4.py
+++ ex4.py
@@ -12,63 +12,71 @@ from file import creer_file_vide,   \
                  defiler
 
 def largeur(arbre):
-    '''renvoie la liste des étiquettes via un parcours en largeur'''
-    file_au_pif = creer_file_vide()
-    lst = []
-    val = arbre
-    lst.append(racine(val))
-    enfiler(file_au_pif, gauche(val))
-    enfiler(file_au_pif, droit(val))
-    while not file_est_vide(file_au_pif):
-        val = defiler(file_au_pif)
-        if not est_vide(val):
-            lst.append(racine(val))
-            enfiler(file_au_pif, gauche(val))
-            enfiler(file_au_pif, droit(val))
-    return lst
+    '''
+    renvoie la liste des étiquettes via un parcours en largeur
+    '''
+    file = creer_file_vide()
+    enfiler(file, arbre)
+    tab = []
+    while not file_est_vide(file) :
+        arbre_en_cours = defiler(file)
+        if not est_vide(arbre_en_cours):
+            tab.append(racine(arbre_en_cours))
+            enfiler(file, gauche(arbre_en_cours))
+            enfiler(file, droit(arbre_en_cours))
+    return tab
 
 def profondeur(arbre, prefixe, infixe, suffixe):
-    '''renvoie un triplet correspondant à la liste des étiquettes via un
-    parcours en profondeur en ordre préfixe, infixe et suffixe'''
-    if est_vide(arbre):
-        return 0
-    prefixe.append(racine(arbre))
-    profondeur(gauche(arbre), prefixe, infixe, suffixe)
-    infixe.append(racine(arbre))
-    profondeur(droit(arbre), prefixe, infixe, suffixe)
-    suffixe.append(racine(arbre))
-    return (prefixe, infixe, suffixe)
+    '''
+    renvoie un triplet correspondant à :
+      - la liste des étiquettes via un parcours en profondeur en ordre préfixe
+      - la liste des étiquettes via un parcours en profondeur en ordre infixe
+      - la liste des étiquettes via un parcours en profondeur en ordre suffixe
+    '''
+    if not est_vide(arbre):
+        prefixe.append(racine(arbre))
+        profondeur(gauche(arbre), prefixe, infixe, suffixe)
+        infixe.append(racine(arbre))
+        profondeur(droit(arbre), prefixe, infixe, suffixe)
+        suffixe.append(racine(arbre))
+    return prefixe, infixe, suffixe
 
 def taille(arbre):
-    '''renvoie la taille de l'arbre binaire'''
+    '''
+    renvoie la taille de l'arbre binaire
+    '''
     if est_vide(arbre):
         return 0
-    t1 = taille(gauche(arbre))
-    t2 = taille(droit(arbre))
-    return 1+t2+t1
+    else:
+        return 1 + taille(gauche(arbre)) + taille(droit(arbre))
 
 def hauteur(arbre):
-    '''renvoie la hauteur de l'arbre binaire'''
+    '''
+    renvoie la hauteur de l'arbre binaire
+    '''
     if est_vide(arbre):
         return -1
-    h1 = hauteur(gauche(arbre))
-    h2 = hauteur(droit(arbre))
-    val = h1
-    if h2 > h1:
-        val = h2
-    return 1+val
+    else:
+        return 1 + max(hauteur(gauche(arbre)), hauteur(droit(arbre)))
 
 def etiquette_presente(arbre, valeur):
-    '''renvoie True lorsque valeur correspond à une étiquette de l'arbre binaire, False sinon'''
+    '''
+    renvoie True lorsque valeur correspond à une étiquette de l'arbre binaire,
+    False sinon
+    '''
     if est_vide(arbre):
         return False
-    if racine(arbre) == valeur:
-        return True
-    return etiquette_presente(gauche(arbre), valeur) or etiquette_presente(droit(arbre), valeur)
-    
-print(largeur(arbre_prof))
-print(profondeur(arbre_prof, [],[],[]))
-print(taille(arbre_prof))
-print(hauteur(arbre_prof))
-print(etiquette_presente(arbre_prof, 'G'))
-print(etiquette_presente(arbre_prof, 'Z'))
+    else:
+        return racine(arbre) == valeur \
+                    or etiquette_presente(gauche(arbre), valeur) \
+                    or etiquette_presente(droit(arbre), valeur)
+
+print('largeur : ', largeur(arbre_prof), end='\n\n')
+prefixe, infixe, suffixe = profondeur(arbre_prof, [], [], [])
+print('profondeur ordre préfixe : ', prefixe, end='\n\n')
+print('profondeur ordre infixe : ', infixe, end='\n\n')
+print('profondeur ordre suffixe : ', suffixe, end='\n\n')
+print('taille = ', taille(arbre_prof), end='\n\n')
+print('hauteur = ', hauteur(arbre_prof), end='\n\n')
+print('G :', etiquette_presente(arbre_prof, 'G'))
+print('Z :', etiquette_presente(arbre_prof, 'Z'))
